6265857054439a032675ad8859406c38224d0a40
vitorpbahia <vitorpeixinhob@gmail.com>
2023-04-19 13:11:35 -0300

    código define posições

--- Batalha_Naval.py/define_posicoes.py
+++ Batalha_Naval.py/define_posicoes.py
@@ -1,1 +1,17 @@
-print('hello World')
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    lista=[]
+    for i in range(tamanho):
+        if orientacao=='vertical':
+            x=[linha+i,coluna]
+            lista.append(x)
+        if orientacao=='horizontal':
+            x=[linha, coluna+i]
+            lista.append(x)
+    return lista
+        
+
+
+
+
+x= define_posicoes(2,4,'vertical', 3)
+print(x)
