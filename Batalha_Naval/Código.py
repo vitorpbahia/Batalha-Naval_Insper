d5623128309e8a95ed1033539e40653d08ebefec
vitorpbahia <vitorpeixinhob@gmail.com>
2023-04-19 13:40:35 -0300

    Código Faz jogada

--- Batalha_Naval/Código.py
+++ Batalha_Naval/Código.py
@@ -23,21 +23,65 @@ tamanho=3
 dicfrota={}
 nomenavio='porta-aviões'
 
+tabuleiro=[
+  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 1, 1, 1, 1, 0],
+  [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 1, 1, 1, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+  [0, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+]
+linha_jogador=1
+coluna_jogador=1
 
 #Código de Preenche frota--------------------------------------------------------------------------------------
 
 
 
 def preenche_frota(dicfrota, nomenavio, linha, coluna, orientacao, tamanho):
+
     if nomenavio in dicfrota:
 
         x=dicfrota[nomenavio]
         x.append(define_posicoes(linha, coluna, orientacao, tamanho))
+
     if nomenavio not in dicfrota:
+
         dicfrota[nomenavio]=[]
         x=dicfrota[nomenavio]
         x.append(define_posicoes(linha, coluna, orientacao, tamanho))
+
     return dicfrota
 
 
 print(preenche_frota(dicfrota, nomenavio, linha, coluna, orientacao, tamanho))
+
+
+#Código Faz jogada-----------------------------------------------------------------------------------------------
+
+def faz_jogada(tabuleiro, linha_jogador, coluna_jogador):
+    if tabuleiro[linha_jogador][coluna_jogador]==1:
+        tabuleiro[linha_jogador][coluna_jogador]='X'
+    if tabuleiro[linha_jogador][coluna_jogador]==0:
+        tabuleiro[linha_jogador][coluna_jogador]='-'
+    
+    return tabuleiro
+
+
+
+tabuleiro=[
+  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 1, 1, 1, 1, 0],
+  [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 1, 1, 1, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+  [0, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+]
