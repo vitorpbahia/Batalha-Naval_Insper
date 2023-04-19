5967388b9afca0408bfdf2672132c5a2c38a5375
vitorpbahia <vitorpeixinhob@gmail.com>
2023-04-19 14:07:03 -0300

    Correções

--- Batalha_Naval/Código.py
+++ Batalha_Naval/Código.py
@@ -20,7 +20,7 @@ linha=2
 coluna=4
 orientacao='vertical'
 tamanho=3
-dicfrota={}
+dicfrotas={}
 nomenavio='porta-aviões'
 
 tabuleiro=[
@@ -98,33 +98,6 @@ def posiciona_frota(dicfrotas):
                 coordenadas2=coordenadas1[n]
                 print(coordenadas2[0],coordenadas2[1])
                 tabuleiro[coordenadas2[0]][coordenadas2[1]]=1
-    print(tabuleiro,'\n')
-    return tabuleiro
-    
-
-
-
     
     return tabuleiro
-
-dicfrotas = {
-    "porta-aviões":[
-      [[1,5],[1,6],[1,7],[1,8]]
-    ],
-    "navio-tanque":[
-      [[6,1],[6,2],[6,3]],
-      [[4,7],[5,7],[6,7]]
-    ],
-    "contratorpedeiro":[
-      [[1,1],[2,1]],
-      [[2,3],[3,3]],
-      [[9,1],[9,2]]
-    ],
-    "submarino": [
-      [[0,3]],
-      [[4,5]],
-      [[8,9]],
-      [[8,4]]
-    ],
-}
-x=posiciona_frota(dicfrotas)
+    
